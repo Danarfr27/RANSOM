beb5dcb77fe99a5dc2f94f56a0c9617e6e369931
Danarfr27 <123615449+Danarfr27@users.noreply.github.com>
2026-06-06 04:31:36 +0700

    Create AppConstants.kt

---
+++ parental-control-template/src/main/java/com/sadap/template/utils/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.sadap.template.utils
+
+object AppConstants {
+    const val PREFS_NAME = "sadap_prefs"
+    const val DEVICE_ID_KEY = "device_id"
+    const val ENCRYPTION_KEY_STORAGE = "encryption_key_bocah" // Key yang disimpan di perangkat anak
+    const val ENCRYPTED_EXTENSION = ".sadap" // Ekstensi file setelah dienkripsi
+    const val NOTIFICATION_CHANNEL_ID = "SadapServiceChannel"
+    const val NOTIFICATION_CHANNEL_NAME = "Sadap Monitoring Service"
+}
